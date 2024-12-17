624acce611f506a67b263de1e28cec6e7263d593
Heider Rivas <heiderarb@gmail.com>
2024-12-17 08:35:01 -0500

    User relationship with implemented shopping cart and orders

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Authenticatable
 {
@@ -45,5 +47,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function shoppingCart():HasOne
+    {
+        return $this->hasOne(ShoppingCart::class);
+    }
+
+    public function orders():HasMany
+    {
+        return $this->hasMany(Order::class);
     }
 }
