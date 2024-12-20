36f11f029fbecb1df36a0654621e528ef2118ddc
Heider Rivas <heiderarb@gmail.com>
2024-12-20 10:51:39 -0500

    Added pagination to product view and performed automated tests for AuthControllerAdded pagination to product view and performed automated tests for AuthController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,15 +8,33 @@ use Illuminate\Http\JsonResponse;
 
 class ProductController extends Controller
 {
-    public function index():JsonResponse
+    public function index(Request $request):JsonResponse
     {
-        $products = Product::with('variants')->get();
+        $products = Product::with('variants');
+
+        if ($request->has('min_price')) {
+            $products->where('price', '>=', $request->input('min_price'));
+        }
+
+        if ($request->has('max_price')) {
+            $products->where('price', '<=', $request->input('max_price'));
+        }
+
+        $perPage = $request->input('per_page', 15);
+        $products = $products->paginate($perPage);
+
+        $products->transform(function ($product) {
+            $product->other_attributes = json_decode($product->other_attributes, true);
+            return $product;
+        });
+
         return response()->json($products,200);
     }
 
     public function show($productId):JsonResponse
     {
         $product = Product::all()->find($productId);
+        $product->other_attributes = json_decode($product->other_attributes,true);
         return response()->json($product,200);
     }
 }
